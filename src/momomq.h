629955739b6d66d6ee99eb220df198a3784c8c71
konyka <konyka@126.com>
2019-04-03 18:46:35 +0800

    add src/momomq.h

---
+++ src/momomq.h
@@ -0,0 +1,23 @@
+/**
+ *===========================================================================
+ *  DarkBlue Engine Source File.
+ *  Copyright (C), DarkBlue Studios.
+ * -------------------------------------------------------------------------
+ *    File name: momomq.h
+ *      Version: v0.0.0
+ *   Created on: Apr 3, 2019 by konyka
+ *       Editor: Sublime Text3
+ *  Description: 
+ * -------------------------------------------------------------------------
+ *      History:
+ *
+ *===========================================================================
+ */
+ 
+
+#ifndef __DARKBLUE_MOMOMQ_H__
+#define __DARKBLUE_MOMOMQ_H__
+
+
+
+#endif /* __DARKBLUE_MOMOMQ_H__ */
