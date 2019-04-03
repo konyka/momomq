9dd918eb192b7d0c67648e781daba86e6dc076fd
konyka <konyka@126.com>
2019-04-03 19:57:39 +0800

    update utility/utility.h

--- src/utility/utility.h
+++ src/utility/utility.h
@@ -15,9 +15,22 @@
  */
  
 
+#ifndef __DARKBLUE_UTILITY_H__
+#define __DARKBLUE_UTILITY_H__
 
+这些函数可以拦截与内存分配相关的操作。
 
+void mm_alloc_init (void);
+void mm_alloc_term (void);
+void *mm_realloc (void *ptr, size_t size);
+void mm_free (void *ptr);
 
+#if defined MM_ALLOC_MONITOR
+#define mm_alloc(size, name) mm_alloc_ (size, name)
+void *mm_alloc_ (size_t size, const char *name);
+#else
+#define mm_alloc(size, name) mm_alloc_(size)
+void *mm_alloc_ (size_t size);
+#endif
 
-
- 
+#endif /* __DARKBLUE_UTILITY_H__ */
