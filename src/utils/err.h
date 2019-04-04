7d3f0c239be720858e9e7803f34d406362e86776
konyka <konyka@126.com>
2019-04-04 10:30:20 +0800

    add err.h

---
+++ src/utils/err.h
@@ -0,0 +1,23 @@
+/**
+ *===========================================================================
+ *  DarkBlue Engine Source File.
+ *  Copyright (C), DarkBlue Studios.
+ * -------------------------------------------------------------------------
+ *    File name: err.h
+ *      Version: v0.0.0
+ *   Created on: Apr 4, 2019 by konyka
+ *       Editor: Sublime Text3
+ *  Description: 
+ * -------------------------------------------------------------------------
+ *      History:
+ *
+ *===========================================================================
+ */
+ 
+
+#ifndef __DARKBLUE_MM_ERR_H__
+#define __DARKBLUE_MM_ERR_H__
+
+
+
+#endif /* __DARKBLUE_MM_ERR_H__ */
