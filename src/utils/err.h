363dace93fde9a1b0c9149b7ff68b6ad72e13963
konyka <konyka@126.com>
2019-04-04 16:18:24 +0800

    add err.h

--- src/utils/err.h
+++ src/utils/err.h
@@ -19,5 +19,24 @@
 #define __DARKBLUE_MM_ERR_H__
 
 
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+//这个属性告诉编译器函数不会返回，这可以用来禁用有关未达到代码路径的错误.
+//this can be used to suppress errors about code paths not being reached.
+#if defined _MSC_VER
+#define MM_NO_RETURN __declspec(noreturn)
+#elif defined __GNUC__
+#define MM_NO_RETURN __attribute__ ((noreturn))
+#else
+#define MM_NO_RETURN
+#endif
+
+
+
+
+
+
 
 #endif /* __DARKBLUE_MM_ERR_H__ */
