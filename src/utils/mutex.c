d9a92fb9aef53871405e1b2cd038eaa31f12ae8e
konyka <konyka@126.com>
2019-04-04 10:01:14 +0800

    update mutex.c : mm_mutex_lock

--- src/utils/mutex.c
+++ src/utils/mutex.c
@@ -53,7 +53,10 @@ void mm_mutex_term (mm_mutex_t *self)
 
 void mm_mutex_lock (mm_mutex_t *self)
 {
-    // to do
+    int rc;
+
+    rc = pthread_mutex_lock (&self->mutex);
+    errnum_assert (rc == 0, rc);
 }
 
 void mm_mutex_unlock (mm_mutex_t *self)
