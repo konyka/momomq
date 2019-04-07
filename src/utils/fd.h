b346f0076ef26773290336db14949d247d21029e
konyka <konyka@126.com>
2019-04-07 22:55:59 +0800

    add fd.h

---
+++ src/utils/fd.h
@@ -0,0 +1,23 @@
+/**
+ *===========================================================================
+ *  DarkBlue Engine Source File.
+ *  Copyright (C), DarkBlue Studios.
+ * -------------------------------------------------------------------------
+ *    File name: fd.h
+ *      Version: v0.0.0
+ *   Created on: Apr 7, 2019 by konyka
+ *       Editor: Sublime Text3
+ *  Description: 
+ * -------------------------------------------------------------------------
+ *      History:
+ *
+ *===========================================================================
+ */
+ 
+
+#ifndef __DARKBLUE_FD_H__
+#define __DARKBLUE_FD_H__
+
+
+
+#endif /* __DARKBLUE_FD_H__ */
