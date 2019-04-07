3c4e89eb442f8b19dfed9ecfece2fddb7cf48932
konyka <konyka@126.com>
2019-04-07 23:11:35 +0800

    update random.h

--- src/utils/random.h
+++ src/utils/random.h
@@ -18,6 +18,19 @@
 #ifndef __DARKBLUE_MM_RANDOM_H__
 #define __DARKBLUE_MM_RANDOM_H__
 
+#include <stddef.h>
 
+/**
+ * [mm_random_seed  种子伪随机数生成器]
+ */
+void mm_random_seed ();
+
+/*  . */
+/**
+ * [mm_random_generate  生成伪随机字节序列]
+ * @param buf [description]
+ * @param len [description]
+ */
+void mm_random_generate (void *buf, size_t len);
 
 #endif /* __DARKBLUE_MM_RANDOM_H__ */
