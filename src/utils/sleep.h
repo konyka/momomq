8498aab97b0f6a5125927541c4198f6ff5a6074a
konyka <konyka@126.com>
2019-04-08 23:00:57 +0800

    update utils/sleep.h

--- src/utils/sleep.h
+++ src/utils/sleep.h
@@ -18,6 +18,10 @@
 #ifndef __DARKBLUE_MM_SLEEP_H__
 #define __DARKBLUE_MM_SLEEP_H__
 
-
+/**
+ * [mm_sleep  不依赖平台的sleep实现]
+ * @param milliseconds [需要sleep的毫秒数]
+ */
+void mm_sleep (int milliseconds);
 
 #endif /* __DARKBLUE_MM_SLEEP_H__ */
