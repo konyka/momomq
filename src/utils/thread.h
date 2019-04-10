369652e162aca38832f698a167d79dcd3c9be894
konyka <konyka@126.com>
2019-04-10 22:26:47 +0800

    update thread.h

--- src/utils/thread.h
+++ src/utils/thread.h
@@ -18,6 +18,18 @@
 #ifndef __DARKBLUE_MM_THREAD_H__
 #define __DARKBLUE_MM_THREAD_H__
 
+/*  不依赖平台的线程实现。 */
 
+typedef void (mm_thread_routine) (void*);
+
+#if defined MM_HAVE_WINDOWS
+#include "thread_win.h"
+#else
+#include "thread_posix.h"
+#endif
+
+void mm_thread_init (struct mm_thread *self,
+    mm_thread_routine *routine, void *arg);
+void mm_thread_term (struct mm_thread *self);
 
 #endif /* __DARKBLUE_MM_THREAD_H__ */
