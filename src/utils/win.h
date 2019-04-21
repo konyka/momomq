8ce22034c73e4595f23457d5b1487fcd80fd3adb
konyka <konyka@126.com>
2019-04-21 22:12:27 +0800

    add utils/win.h

---
+++ src/utils/win.h
@@ -0,0 +1,26 @@
+/**
+ *===========================================================================
+ *  None Source File.
+ *  Copyright (C), DarkBlue Studios.
+ * -------------------------------------------------------------------------
+ *    File name: win.h
+ *      Version: v0.0.0
+ *   Created on: 2019-04-21 22:11:08 by konyka
+ *  Modified by: konyka
+ *Modified time: 2019-04-21 22:11:08
+ *       Editor: Sublime Text3
+ *        Email: 
+ *  Description: 
+ * -------------------------------------------------------------------------
+ *      History: 
+ *
+ *===========================================================================
+ */
+ 
+
+#ifndef __DARKBLUE_MM_WIN_H__
+#define __DARKBLUE_MM_WIN_H__
+
+
+
+#endif /* __DARKBLUE_MM_WIN_H__ */
