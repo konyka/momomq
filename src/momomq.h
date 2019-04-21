3680a5ec916ced2ec6df3e67febc4469127a8a9c
konyka <konyka@126.com>
2019-04-21 22:19:02 +0800

    update momomq.h

---
+++ src/momomq.h
@@ -0,0 +1,56 @@
+/**
+ *===========================================================================
+ *  DarkBlue Engine Source File.
+ *  Copyright (C), DarkBlue Studios.
+ * -------------------------------------------------------------------------
+ *    File name: momomq.h
+ *      Version: v0.0.0
+ *   Created on: Apr 3, 2019 by konyka
+ *       Editor: Sublime Text3
+ *  Description: 
+ * -------------------------------------------------------------------------
+ *      History:
+ *
+ *===========================================================================
+ */
+ 
+
+#ifndef __DARKBLUE_MM_MOMOMQ_H__
+#define __DARKBLUE_MM_MOMOMQ_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <errno.h>
+#include <stddef.h>
+#include <stdint.h>
+
+ /*
+  *Socket .定义
+  */                                                       
+
+
+struct mm_iovec {
+    size_t iov_len;
+    void *iov_base;
+};
+
+struct mm_msg_header {
+    int msg_iovlen;
+    struct mm_iovec *msg_iov;
+    size_t msg_controllen;  
+    void *msg_control;
+};
+
+struct mm_cmsg_header {
+    size_t cmsg_len;
+    int cmsg_level;
+    int cmsg_type;
+};
+
+#ifdef __cplusplus
+}
+#endif  
+
+#endif /* __DARKBLUE_MM_MOMOMQ_H__ */
