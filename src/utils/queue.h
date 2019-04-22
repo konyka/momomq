dade913b7efc021e4204e24209db64ace23ef9e7
konyka <konyka@126.com>
2019-04-22 18:17:35 +0800

    add utils/queue.h

---
+++ src/utils/queue.h
@@ -0,0 +1,26 @@
+/**
+ *===========================================================================
+ *  None Source File.
+ *  Copyright (C), DarkBlue Studios.
+ * -------------------------------------------------------------------------
+ *    File name: queue.h
+ *      Version: v0.0.0
+ *   Created on: 2019-04-22 18:16:25 by konyka
+ *  Modified by: konyka
+ *Modified time: 2019-04-22 18:16:25
+ *       Editor: Sublime Text3
+ *        Email: 
+ *  Description: 
+ * -------------------------------------------------------------------------
+ *      History: 
+ *
+ *===========================================================================
+ */
+ 
+
+#ifndef __DARKBLUE_MM_QUEUE_H__
+#define __DARKBLUE_MM_QUEUE_H__
+
+
+
+#endif /* __DARKBLUE_MM_QUEUE_H__ */
